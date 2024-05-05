92e1c6dae9ca37f8cb5c23496d68b8b01be26ef8
MuhammadAlfianIzzah <alfianizzah6@gmail.com>
2024-05-06 07:37:40 +0800

    tambah random quote

--- src/Helper/StringHelper.php
+++ src/Helper/StringHelper.php
@@ -8,4 +8,20 @@ class StringHelper
     {
         return "hello World";
     }
+
+    public static function randomQuote()
+    {
+        $jsonData = file_get_contents(__DIR__ . "/../Dataset/quotes.json");
+        if ($jsonData === false) {
+            return "json failed";
+        } else {
+            $data = json_decode($jsonData, true);
+            if ($data === null) {
+                return "data json kosong";
+            } else {
+                $randomIndex = array_rand($data);
+                return $data[$randomIndex]["quote"];
+            }
+        }
+    }
 }
